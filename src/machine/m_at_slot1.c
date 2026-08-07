73208cea810f4781a115dfa082129f5e91d5aa7d
James Weidner <jamesr@theweidners.us>
2026-08-06 20:20:47 -0400

    Add ABIT BE6-II revisions 1.2 and 2.0 HPT370 support

--- src/machine/m_at_slot1.c
+++ src/machine/m_at_slot1.c
@@ -851,180 +851,108 @@ machine_at_bf6_init(const machine_t *model)
     return ret;
 }
 
-static const device_config_t be6ii_config[] = {
-    // clang-format off
+static const device_config_t be6ii_12_config[] = {
     {
-        .name           = "bios",
-        .description    = "BIOS Version",
-        .type           = CONFIG_BIOS,
-        .default_string = "be6ii_70",
-        .default_int    = 0,
-        .file_filter    = NULL,
-        .spinner        = { 0 },
-        .selection      = { { 0 } },
-        .bios           = {
-            {
-                .name          = "Award Modular BIOS v6.00PG - Revision 70",
-                .internal_name = "be6ii_70",
-                .bios_type     = BIOS_NORMAL,
-                .files_no      = 1,
-                .local         = 0,
-                .size          = 262144,
-                .files         = { "roms/machines/be6ii/Beh_70.bin", "" }
-            },
-            {
-                .name          = "Award Modular BIOS v6.00PG - Revision NP",
-                .internal_name = "be6ii_np",
-                .bios_type     = BIOS_NORMAL,
-                .files_no      = 1,
-                .local         = 0,
-                .size          = 262144,
-                .files         = { "roms/machines/be6ii/BEH_NP.BIN", "" }
-            },
-            {
-                .name          = "Award Modular BIOS v6.00PG - Revision NY",
-                .internal_name = "be6ii_ny",
-                .bios_type     = BIOS_NORMAL,
-                .files_no      = 1,
-                .local         = 0,
-                .size          = 262144,
-                .files         = { "roms/machines/be6ii/BEH_NY.BIN", "" }
-            },
-            {
-                .name          = "Award Modular BIOS v6.00PG - Revision NZ",
-                .internal_name = "be6ii_nz",
-                .bios_type     = BIOS_NORMAL,
-                .files_no      = 1,
-                .local         = 0,
-                .size          = 262144,
-                .files         = { "roms/machines/be6ii/BEH_NZ.BIN", "" }
-            },
-            {
-                .name          = "Award Modular BIOS v6.00PG - Revision PO",
-                .internal_name = "be6ii_po",
-                .bios_type     = BIOS_NORMAL,
-                .files_no      = 1,
-                .local         = 0,
-                .size          = 262144,
-                .files         = { "roms/machines/be6ii/BEH_PO.BIN", "" }
-            },
-            {
-                .name          = "Award Modular BIOS v6.00PG - Revision QJ",
-                .internal_name = "be6ii_qj",
-                .bios_type     = BIOS_NORMAL,
-                .files_no      = 1,
-                .local         = 0,
-                .size          = 262144,
-                .files         = { "roms/machines/be6ii/BEH_QJ.BIN", "" }
-            },
-            {
-                .name          = "Award Modular BIOS v6.00PG - Revision RV",
-                .internal_name = "be6ii_rv",
-                .bios_type     = BIOS_NORMAL,
-                .files_no      = 1,
-                .local         = 0,
-                .size          = 262144,
-                .files         = { "roms/machines/be6ii/BEH_RV.BIN", "" }
-            },
-            {
-                .name          = "Award Modular BIOS v6.00PG - Revision SH",
-                .internal_name = "be6ii_sh",
-                .bios_type     = BIOS_NORMAL,
-                .files_no      = 1,
-                .local         = 0,
-                .size          = 262144,
-                .files         = { "roms/machines/be6ii/BEH_SH.BIN", "" }
-            },
-            {
-                .name          = "Award Modular BIOS v6.00PG - Revision UH",
-                .internal_name = "be6ii_uh",
-                .bios_type     = BIOS_NORMAL,
-                .files_no      = 1,
-                .local         = 0,
-                .size          = 262144,
-                .files         = { "roms/machines/be6ii/BEH_UH.BIN", "" }
-            },
-            {
-                .name          = "Award Modular BIOS v6.00PG - Revision VN",
-                .internal_name = "be6ii_vn",
-                .bios_type     = BIOS_NORMAL,
-                .files_no      = 1,
-                .local         = 0,
-                .size          = 262144,
-                .files         = { "roms/machines/be6ii/beh_vn.Bin", "" }
-            },
-            {
-                .name          = "Award Modular BIOS v6.00PG - Revision XU",
-                .internal_name = "be6ii_xu",
-                .bios_type     = BIOS_NORMAL,
-                .files_no      = 1,
-                .local         = 0,
-                .size          = 262144,
-                .files         = { "roms/machines/be6ii/BEH_XU.BIN", "" }
-            },
+        .name = "bios", .description = "BIOS Version", .type = CONFIG_BIOS,
+        .default_string = "be6ii12_71",
+        .bios = {
+            { .name = "Award Modular BIOS v6.00PG - Revision 71", .internal_name = "be6ii12_71",
+              .bios_type = BIOS_NORMAL, .files_no = 1, .size = 262144,
+              .files = { "roms/machines/be6ii_12/Bep_71.bin", "" } },
+            { .name = "Award Modular BIOS v6.00PG - Revision RP", .internal_name = "be6ii12_rp",
+              .bios_type = BIOS_NORMAL, .files_no = 1, .size = 262144,
+              .files = { "roms/machines/be6ii_12/BEP_RP.BIN", "" } },
+            { .name = "Award Modular BIOS v6.00PG - Revision UJ", .internal_name = "be6ii12_uj",
+              .bios_type = BIOS_NORMAL, .files_no = 1, .size = 262144,
+              .files = { "roms/machines/be6ii_12/BEP_UJ.BIN", "" } },
+            { .name = "Award Modular BIOS v6.00PG - Revision XV", .internal_name = "be6ii12_xv",
+              .bios_type = BIOS_NORMAL, .files_no = 1, .size = 262144,
+              .files = { "roms/machines/be6ii_12/BEP_XV.BIN", "" } },
+            { .name = "Award Modular BIOS v6.00PG - Revision ZX", .internal_name = "be6ii12_zx",
+              .bios_type = BIOS_NORMAL, .files_no = 1, .size = 262144,
+              .files = { "roms/machines/be6ii_12/bep_zx.BIN", "" } },
             { .files_no = 0 }
         }
     },
     { .name = "", .description = "", .type = CONFIG_END }
-    // clang-format on
 };
 
-const device_t be6ii_device = {
-    .name          = "ABIT AB-BE6-II",
-    .internal_name = "be6ii",
-    .flags         = 0,
-    .local         = 0,
-    .init          = NULL,
-    .close         = NULL,
-    .reset         = NULL,
-    .available     = NULL,
-    .speed_changed = NULL,
-    .force_redraw  = NULL,
-    .config        = be6ii_config
+const device_t be6ii_12_device = {
+    .name = "ABIT AB-BE6-II revision 1.2", .internal_name = "be6ii_12", .config = be6ii_12_config
 };
 
-int
-machine_at_be6ii_init(const machine_t *model)
+static const device_config_t be6ii_20_config[] = {
+    {
+        .name = "bios", .description = "BIOS Version", .type = CONFIG_BIOS,
+        .default_string = "be6ii20_xk",
+        .bios = {
+            { .name = "Award Modular BIOS v6.00PG - Revision XK", .internal_name = "be6ii20_xk",
+              .bios_type = BIOS_NORMAL, .files_no = 1, .size = 262144,
+              .files = { "roms/machines/be6ii_20/BER_XK.BIN", "" } },
+            { .name = "Award Modular BIOS v6.00PG - Revision UK", .internal_name = "be6ii20_uk",
+              .bios_type = BIOS_NORMAL, .files_no = 1, .size = 262144,
+              .files = { "roms/machines/be6ii_20/BER_UK.BIN", "" } },
+            { .name = "Award Modular BIOS v6.00PG - Revision VI", .internal_name = "be6ii20_vi",
+              .bios_type = BIOS_NORMAL, .files_no = 1, .size = 262144,
+              .files = { "roms/machines/be6ii_20/BER_VI.BIN", "" } },
+            { .files_no = 0 }
+        }
+    },
+    { .name = "", .description = "", .type = CONFIG_END }
+};
+
+const device_t be6ii_20_device = {
+    .name = "ABIT AB-BE6-II revision 2.0", .internal_name = "be6ii_20", .config = be6ii_20_config
+};
+
+static int
+machine_at_be6ii_hpt370_init(const machine_t *model, const device_t *controller)
 {
-    int         ret = 0;
+    int ret;
     const char *fn;
 
     if (!device_available(model->device))
-        return ret;
-
+        return 0;
     device_context(model->device);
-    fn  = device_get_bios_file(machine_get_device(machine), device_get_config_bios("bios"), 0);
+    fn = device_get_bios_file(machine_get_device(machine), device_get_config_bios("bios"), 0);
     ret = bios_load_linear(fn, 0x000c0000, 262144, 0);
     device_context_restore();
-
     if (bios_only || !ret)
         return ret;
 
     machine_at_common_init(model);
-
     pci_init(PCI_CONFIG_TYPE_1);
     pci_register_slot(0x00, PCI_CARD_NORTHBRIDGE, 0, 0, 0, 0);
     pci_register_slot(0x07, PCI_CARD_SOUTHBRIDGE, 1, 2, 3, 4);
-    pci_register_slot(0x09, PCI_CARD_NORMAL,      1, 2, 3, 4);
-    pci_register_slot(0x0B, PCI_CARD_NORMAL,      2, 3, 4, 1);
-    pci_register_slot(0x0D, PCI_CARD_NORMAL,      3, 4, 1, 2);
-    pci_register_slot(0x0F, PCI_CARD_NORMAL,      4, 1, 2, 3);
-    pci_register_slot(0x11, PCI_CARD_NORMAL,      2, 3, 4, 1);
-    pci_register_slot(0x13, PCI_CARD_IDE,         3, 4, 1, 2);
-    pci_register_slot(0x01, PCI_CARD_AGPBRIDGE,   1, 2, 3, 4);
-
+    pci_register_slot(0x09, PCI_CARD_NORMAL, 1, 2, 3, 4);
+    pci_register_slot(0x0b, PCI_CARD_NORMAL, 2, 3, 4, 1);
+    pci_register_slot(0x0d, PCI_CARD_NORMAL, 3, 4, 1, 2);
+    pci_register_slot(0x0f, PCI_CARD_NORMAL, 4, 1, 2, 3);
+    pci_register_slot(0x11, PCI_CARD_NORMAL, 2, 3, 4, 1);
+    pci_register_slot(0x13, PCI_CARD_IDE, 3, 4, 1, 2);
+    pci_register_slot(0x01, PCI_CARD_AGPBRIDGE, 1, 2, 3, 4);
     device_add(&i440bx_device);
     device_add(&piix4e_device);
     device_add_params(&w83977_device, (void *) (W83977EF | W83977_AMI | W83977_NO_NVR));
-    device_add(&ide_hpt366_ter_qua_onboard_device);
+    device_add(controller);
     device_add(&sst_flash_39sf020_device);
     spd_register(SPD_TYPE_SDRAM, 0x7, 256);
-    device_add(&w83782d_device); /* fans: Chassis, Power, CPU; temperatures: System, CPU, unused */
+    device_add(&w83782d_device);
     hwm_values.temperatures[2] = 0;
-
     return ret;
 }
 
+int
+machine_at_be6ii_12_init(const machine_t *model)
+{
+    return machine_at_be6ii_hpt370_init(model, &ide_hpt370_ter_qua_onboard_device);
+}
+
+int
+machine_at_be6ii_20_init(const machine_t *model)
+{
+    return machine_at_be6ii_hpt370_init(model, &ide_hpt370_20_ter_qua_onboard_device);
+}
 static const device_config_t bx6_config[] = {
     // clang-format off
     {
--- src/machine/machine_table.c
+++ src/machine/machine_table.c
@@ -22149,56 +22149,38 @@ const machine_t machines[] = {
         .net_device               = NULL,
         .aliases                  = { "" }
     },
-    /* Has a Winbond W83977EF Super I/O chip with on-chip KBC with AMIKey-2 KBC
-       firmware and an on-board HighPoint HPT366 IDE controller. */
+    /* Later BE6-II PCB revision with an on-board HighPoint HPT370 IDE controller. */
     {
-        .name              = "[i440BX] ABIT AB-BE6-II",
-        .internal_name     = "be6ii",
-        .type              = MACHINE_TYPE_SLOT1,
-        .chipset           = MACHINE_CHIPSET_INTEL_440BX,
-        .init              = machine_at_be6ii_init,
-        .p1_handler        = machine_generic_p1_handler,
-        .gpio_handler      = NULL,
-        .available_flag    = MACHINE_AVAILABLE,
-        .gpio_acpi_handler = NULL,
-        .cpu               = {
-            .package     = CPU_PKG_SLOT1,
-            .block       = CPU_BLOCK_NONE,
-            .min_bus     = 66666667,
-            .max_bus     = 133333333,
-            .min_voltage = 1300,
-            .max_voltage = 3500,
-            .min_multi   = 1.5,
-            .max_multi   = 8.0
-        },
+        .name = "[i440BX] ABIT AB-BE6-II revision 1.2", .internal_name = "be6ii_12",
+        .type = MACHINE_TYPE_SLOT1, .chipset = MACHINE_CHIPSET_INTEL_440BX,
+        .init = machine_at_be6ii_12_init, .p1_handler = machine_generic_p1_handler,
+        .available_flag = MACHINE_AVAILABLE,
+        .cpu = { .package = CPU_PKG_SLOT1, .block = CPU_BLOCK_NONE, .min_bus = 66666667,
+                 .max_bus = 133333333, .min_voltage = 1300, .max_voltage = 3500,
+                 .min_multi = 1.5, .max_multi = 8.0 },
         .bus_flags = MACHINE_PS2_AGP | MACHINE_BUS_USB,
-        .flags     = MACHINE_IDE_QUAD | MACHINE_APM | MACHINE_ACPI | MACHINE_USB,
-        .ram       = {
-            .min  = 8192,
-            .max  = 786432,
-            .step = 8192
-        },
-        .nvrmask                  = 255,
-        .jumpered_ecp_dma         = 0,
-        .default_jumpered_ecp_dma = -1,
-        .kbc_device               = NULL,
-        .kbc_params               = 0x00000000,
-        .nvr_device               = NULL,
-        .nvr_params               = 0x00000000,
-        .sio_device               = NULL,
-        .sio_params               = 0x00000000,
-        .kbc_p1                   = 0x00000cf0,
-        .gpio                     = 0xffffffff,
-        .gpio_acpi                = 0xffffffff,
-        .device                   = &be6ii_device,
-        .kbd_device               = NULL,
-        .fdc_device               = NULL,
-        .vid_device               = NULL,
-        .snd_device               = NULL,
-        .net_device               = NULL,
-        .aliases                  = { "" }
+        .flags = MACHINE_IDE_QUAD | MACHINE_APM | MACHINE_ACPI | MACHINE_USB,
+        .ram = { .min = 8192, .max = 786432, .step = 8192 },
+        .nvrmask = 255, .default_jumpered_ecp_dma = -1, .kbc_p1 = 0x00000cf0,
+        .gpio = 0xffffffff, .gpio_acpi = 0xffffffff, .device = &be6ii_12_device,
+        .aliases = { "" }
     },
-    /* Has a Winbond W83977TF Super I/O chip with on-chip KBC with AMIKey-2 KBC
+    /* Final BE6-II PCB revision with an on-board HighPoint HPT370 IDE controller. */
+    {
+        .name = "[i440BX] ABIT AB-BE6-II revision 2.0", .internal_name = "be6ii_20",
+        .type = MACHINE_TYPE_SLOT1, .chipset = MACHINE_CHIPSET_INTEL_440BX,
+        .init = machine_at_be6ii_20_init, .p1_handler = machine_generic_p1_handler,
+        .available_flag = MACHINE_AVAILABLE,
+        .cpu = { .package = CPU_PKG_SLOT1, .block = CPU_BLOCK_NONE, .min_bus = 66666667,
+                 .max_bus = 133333333, .min_voltage = 1300, .max_voltage = 3500,
+                 .min_multi = 1.5, .max_multi = 8.0 },
+        .bus_flags = MACHINE_PS2_AGP | MACHINE_BUS_USB,
+        .flags = MACHINE_IDE_QUAD | MACHINE_APM | MACHINE_ACPI | MACHINE_USB,
+        .ram = { .min = 8192, .max = 786432, .step = 8192 },
+        .nvrmask = 255, .default_jumpered_ecp_dma = -1, .kbc_p1 = 0x00000cf0,
+        .gpio = 0xffffffff, .gpio_acpi = 0xffffffff, .device = &be6ii_20_device,
+        .aliases = { "" }
+    },    /* Has a Winbond W83977TF Super I/O chip with on-chip KBC with AMIKey-2 KBC
        firmware. */
     {
         .name              = "[i440BX] ABIT AB-BX6",
